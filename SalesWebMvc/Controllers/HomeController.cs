c389ea939038c10ebb66aad0cc00882dbf498b19
Claudio Lopes <40311001+ClaudioELopes@users.noreply.github.com>
2019-02-19 13:30:33 -0300

    First Model-Controller-View - Department

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -33,12 +33,12 @@ namespace SalesWebMvc.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
+        }   
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new SalesWebMvc.Models.ViewModels.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
